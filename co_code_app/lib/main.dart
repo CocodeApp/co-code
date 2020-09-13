62b6f481340d34cbc129618845050f234fafc945
LatifahAlomar <55920491+LatifahAlomar@users.noreply.github.com>
2020-09-13 16:12:13 +0300

    home page and view profile page

--- co_code_app/lib/main.dart
+++ co_code_app/lib/main.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
 import 'postIdeaForm.dart';
+import 'homePage.dart' as home;
+import 'myProjectsPage.dart' as projects;
+import 'viewProject.dart' as view;
 
 void main() {
-  runApp(MyApp());
+  runApp(home.homePage());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Co-Code',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: PostIdeaFormPage(title: 'Post Idea'),
-    );
+    return MaterialApp(home: Scaffold(appBar: AppBar(), body: home.IdeaCard()));
   }
 }
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Co-Code',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: PostIdeaFormPage(title: 'Post Idea'),
+//     );
+//   }
+// }
