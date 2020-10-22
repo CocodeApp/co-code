dda37ba2662b3bca682ca58abf8fd1ded86eb89e
LatifahAlomar <55920491+LatifahAlomar@users.noreply.github.com>
2020-10-22 23:35:27 +0300

    add event

--- cocode/lib/features/addEvent.dart
+++ cocode/lib/features/addEvent.dart
@@ -1,152 +1,57 @@
-import 'dart:async';
-import 'dart:convert' show json;
-
-import "package:http/http.dart" as http;
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:googleapis/calendar/v3.dart';
 
-GoogleSignIn _googleSignIn = GoogleSignIn(
-  scopes: <String>[
-    'email',
-    'https://www.googleapis.com/auth/contacts.readonly',
-  ],
-);
+import 'GoogleAuthClient.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      title: 'Google Sign In',
-      home: SignInDemo(),
-    ),
-  );
+class signIn extends StatefulWidget {
+  @override
+  _signInState createState() => _signInState();
 }
 
-class SignInDemo extends StatefulWidget {
-  @override
-  State createState() => SignInDemoState();
-}
-
-class SignInDemoState extends State<SignInDemo> {
-  GoogleSignInAccount _currentUser;
-  String _contactText;
-
-  @override
-  void initState() {
-    super.initState();
-    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account) {
-      setState(() {
-        _currentUser = account;
-      });
-      if (_currentUser != null) {
-        _handleGetContact();
-      }
-    });
-    _googleSignIn.signInSilently();
-  }
-
-  Future<void> _handleGetContact() async {
-    setState(() {
-      _contactText = "Loading contact info...";
-    });
-    final http.Response response = await http.get(
-      'https://people.googleapis.com/v1/people/me/connections'
-      '?requestMask.includeField=person.names',
-      headers: await _currentUser.authHeaders,
-    );
-    if (response.statusCode != 200) {
-      setState(() {
-        _contactText = "People API gave a ${response.statusCode} "
-            "response. Check logs for details.";
-      });
-      print('People API ${response.statusCode} response: ${response.body}');
-      return;
-    }
-    final Map<String, dynamic> data = json.decode(response.body);
-    final String namedContact = _pickFirstNamedContact(data);
-    setState(() {
-      if (namedContact != null) {
-        _contactText = "I see you know $namedContact!";
-      } else {
-        _contactText = "No contacts to display.";
-      }
-    });
-  }
-
-  String _pickFirstNamedContact(Map<String, dynamic> data) {
-    final List<dynamic> connections = data['connections'];
-    final Map<String, dynamic> contact = connections?.firstWhere(
-      (dynamic contact) => contact['names'] != null,
-      orElse: () => null,
-    );
-    if (contact != null) {
-      final Map<String, dynamic> name = contact['names'].firstWhere(
-        (dynamic name) => name['displayName'] != null,
-        orElse: () => null,
-      );
-      if (name != null) {
-        return name['displayName'];
-      }
-    }
-    return null;
-  }
-
-  Future<void> _handleSignIn() async {
-    try {
-      await _googleSignIn.signIn();
-    } catch (error) {
-      print(error);
-    }
-  }
-
-  Future<void> _handleSignOut() => _googleSignIn.disconnect();
-
-  Widget _buildBody() {
-    if (_currentUser != null) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          ListTile(
-            leading: GoogleUserCircleAvatar(
-              identity: _currentUser,
-            ),
-            title: Text(_currentUser.displayName ?? ''),
-            subtitle: Text(_currentUser.email ?? ''),
-          ),
-          const Text("Signed in successfully."),
-          Text(_contactText ?? ''),
-          RaisedButton(
-            child: const Text('SIGN OUT'),
-            onPressed: _handleSignOut,
-          ),
-          RaisedButton(
-            child: const Text('REFRESH'),
-            onPressed: _handleGetContact,
-          ),
-        ],
-      );
-    } else {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          const Text("You are not currently signed in."),
-          RaisedButton(
-            child: const Text('SIGN IN'),
-            onPressed: _handleSignIn,
-          ),
-        ],
-      );
-    }
-  }
-
+class _signInState extends State<signIn> {
   @override
   Widget build(BuildContext context) {
+    Future<void> signIn() async {
+      final googleSignIn =
+          GoogleSignIn.standard(scopes: [CalendarApi.CalendarScope]);
+      final GoogleSignInAccount account = await googleSignIn.signIn();
+      print("User account $account");
+      final authHeaders = await account.authHeaders;
+      final authenticateClient = GoogleAuthClient(authHeaders);
+      final driveApi = CalendarApi(authenticateClient);
+      Event event = Event();
+      event.summary = "test";
+      EventDateTime start = new EventDateTime(); //Setting start time
+      start.dateTime = new DateTime(2020 - 10 - 23, 6, 30);
+      start.timeZone = "GMT+05:00";
+      event.start = start;
+
+      EventDateTime end = new EventDateTime(); //setting end time
+      end.timeZone = "GMT+05:00";
+      end.dateTime = start.dateTime = new DateTime(2020 - 10 - 23, 7, 30);
+      event.end = end;
+      var calendar = CalendarApi(authenticateClient);
+      String calendarId = "primary";
+      calendar.events.insert(event, calendarId).then((value) {
+        print("ADDEDDD_________________${value.status}");
+        if (value.status == "confirmed") {
+          print('Event added in google calendar');
+        } else {
+          print("Unable to add event in google calendar");
+        }
+      });
+    }
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Google Sign In'),
+      body: Center(
+        child: FlatButton(
+          child: Text("sign"),
+          onPressed: () async {
+            signIn();
+          },
         ),
-        body: ConstrainedBox(
-          constraints: const BoxConstraints.expand(),
-          child: _buildBody(),
-        ));
+      ),
+    );
   }
 }
