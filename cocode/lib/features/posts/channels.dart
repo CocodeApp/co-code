6160a52b15e4fc06abfe3ccbcd380c0c404a2d1c
Najd Alsughaiyer <70834431+plateauh@users.noreply.github.com>
2020-10-23 23:48:06 +0300

    add channel button with its alert

--- cocode/lib/features/posts/channels.dart
+++ cocode/lib/features/posts/channels.dart
@@ -50,60 +50,106 @@ class _channelsState extends State<channels> {
                     );
                   } // has no data if closing
                   List channels = snapshot.data.docs;
-                  return ListView.builder(
-                      physics: const AlwaysScrollableScrollPhysics(),
-                      scrollDirection: Axis.vertical,
-                      itemCount: snapshot.data.docs.length,
-                      itemBuilder: (context, index){
-                        return Container(
-                          height: 120.0,
-                          margin: const EdgeInsets.symmetric(
-                            vertical: 16.0,
-                            horizontal: 24.0,
-                          ),
-                          child: Stack(
-                            children: [
-                              Container(
-                                clipBehavior: Clip.hardEdge,
-                                alignment: Alignment.center,
-                                height: 124.0,
-                                decoration: new BoxDecoration(
-                                  color: new Color(0xFFD1DDED),
-                                  shape: BoxShape.rectangle,
-                                  borderRadius: new BorderRadius.circular(8.0),
-                                  boxShadow: <BoxShadow>[
-                                    new BoxShadow(
-                                      color: Colors.black12,
-                                      blurRadius: 10.0,
-                                      offset: new Offset(0.0, 10.0),
-                                    ),
-                                  ],
-                                ),
-                                child: InkWell(
-                                  splashColor: Colors.blue.withAlpha(30),
-                                  onTap: (){
-                                    Navigator.push(context,
-                                        MaterialPageRoute(
-                                      builder: (context) => Chat(
-                                          projectId: widget.projectId,
-                                          channleId: channels[index].id),
-                                    )
-                                    );
-                                  },
-                                  child: ListTile(
-                                    title: Text(channels[index].data()['name'], textAlign: TextAlign.center,),
-                                  ),
-                              ),
-                            ),
-                          ]),
-                        );
-                      });
+                  return channelsList(channels);
                 }
             ),
-          ), ]
-        ),
+          ),
+            FloatingActionButton(
+                backgroundColor: Colors.deepOrangeAccent,
+                child: Icon(Icons.add),
+                onPressed: (){
+                  addChannel();
+                }
+                )
+          ]),
       ),
     ); // StreamBuilder
+  }
+
+  Widget channelsList(List channels){
+    return ListView.builder(
+        physics: const AlwaysScrollableScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        itemCount: channels.length,
+        itemBuilder: (context, index){
+          return Container(
+            height: 120.0,
+            margin: const EdgeInsets.symmetric(
+              vertical: 16.0,
+              horizontal: 24.0,
+            ),
+            child: Stack(
+                children: [
+                  Container(
+                    clipBehavior: Clip.hardEdge,
+                    alignment: Alignment.center,
+                    height: 124.0,
+                    decoration: new BoxDecoration(
+                      color: new Color(0xFFD1DDED),
+                      shape: BoxShape.rectangle,
+                      borderRadius: new BorderRadius.circular(8.0),
+                      boxShadow: <BoxShadow>[
+                        new BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 10.0,
+                          offset: new Offset(0.0, 10.0),
+                        ),
+                      ],
+                    ),
+                    child: InkWell(
+                      splashColor: Colors.blue.withAlpha(30),
+                      onTap: (){
+                        Navigator.push(context,
+                            MaterialPageRoute(
+                              builder: (context) => Chat(
+                                  projectId: widget.projectId,
+                                  channleId: channels[index].id),
+                            )
+                        );
+                      },
+                      child: ListTile(
+                        title: Text(
+                          channels[index].data()['name'],
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ),
+                ]),
+          );
+        });
+  }
+
+  Future<void> addChannel() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add new channel'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Channel name:'),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'e.g. Development'
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Add'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
 
