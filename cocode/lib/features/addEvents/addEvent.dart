7e9f9105d74b07362e2f9d7511f59fa5a5103d56
LatifahAlomar <55920491+LatifahAlomar@users.noreply.github.com>
2020-10-23 12:25:17 +0300

    addevent

--- cocode/lib/features/addEvents/addEvent.dart
+++ cocode/lib/features/addEvents/addEvent.dart
@@ -12,25 +12,33 @@ class addEvent extends StatefulWidget {
 class _addEventState extends State<addEvent> {
   @override
   Widget build(BuildContext context) {
-    Future<void> addEvent() async {
+    Future<void> addEvent(String summary, DateTime startTime, DateTime endTime,
+        String location, List attendees) async {
       final googleSignIn =
           GoogleSignIn.standard(scopes: [CalendarApi.CalendarScope]);
       final GoogleSignInAccount account = await googleSignIn.signIn();
       print("User account $account");
       final authHeaders = await account.authHeaders;
       final authenticateClient = GoogleAuthClient(authHeaders);
-      final driveApi = CalendarApi(authenticateClient);
-      Event event = Event();
-      event.summary = "test";
+
+      Event event = Event(); //event object to be inserted
+      event.summary = summary; //summary or event title
+
       EventDateTime start = new EventDateTime(); //Setting start time
-      start.dateTime = new DateTime(2020, 10, 23, 6, 30);
-      start.timeZone = "GMT+05:00";
+      start.dateTime = DateTime(2020, 10, 23, 7, 20);
+      start.timeZone = "GMT+3:00";
       event.start = start;
 
       EventDateTime end = new EventDateTime(); //setting end time
-      end.timeZone = "GMT+05:00";
-      end.dateTime = start.dateTime = new DateTime(2020, 10, 23, 7, 30);
+      end.timeZone = "GMT+03:00";
+      end.dateTime = DateTime(2020, 10, 23, 7, 20);
+
       event.end = end;
+      // Map<dynamic, dynamic> map = {"email": "latifah.a.alomar@gmail.com"};
+      // event.attendees = [EventAttendee.fromJson(map)]; //list of attendens
+
+      event.location = location;
+
       var calendar = CalendarApi(authenticateClient);
       String calendarId = "primary";
       calendar.events.insert(event, calendarId).then((value) {
